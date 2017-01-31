bb422222ebac7da94cc200fb6998de70a3e7e037
Govind Prakash Bhone <bhonegovind@gmail.com>
2017-01-31 14:44:08 +0530

    Create Exercise.1.10.scala

---
+++ src/main/scala/Exercise.1.10.scala
@@ -0,0 +1,204 @@
+/*Exercise 1.10. The following procedure computes a mathematical function called Ackermann's function.
+(define (A x y)
+(cond ((= y 0) 0)
+((= x 0) (* 2 y))
+((= y 1) 2)
+(else (A (- x 1)
+(A x (- y 1))))))
+What are the values of the following expressions?
+(A 1 10)
+(A 2 4)
+(A 3 3)
+Consider the following procedures, where A is the procedure defined above:
+(define (f n) (A 0 n))
+(define (g n) (A 1 n))
+(define (h n) (A 2 n))
+(define (k n) (* 5 n n))
+Give concise mathematical definitions for the functions computed by the procedures f , g , and h for
+positive integer values of n. For example, (k n) computes 5 n2.
+Answer :
+This problem require linear recursive substitution as follows (we can observe this using shape of
+expansion followed by contraction of operation graph )
+(A 1 10) =>
+(A 1 10)
+(A 0 (A 1 9))
+(A 0 (A 0 (A 1 8)))
+(A 0 (A 0 (A 0 (A 1 7))))
+(A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+(A 0 (A 0 (A 0 (A 0 64))))
+(A 0 (A 0 (A 0 128)))
+(A 0 (A 0 256))
+(A 0 512)
+1024
+(A 2 4) =>
+(A 2 4)
+(A 1 (A 2 3))
+(A 1 (A 1 (A 2 2)))
+(A 1 (A 1 (A 1 (A 2 1))))
+(A 1 (A 1 (A 1 2)))
+(A 1 (A 1 (A 0 (A 1 1))))
+(A 1 (A 1 (A 0 2)))
+(A 1 (A 1 4))
+(A 1 (A 0 (A 1 3)))
+(A 1 (A 0 (A 0 (A 1 2))))
+(A 1 (A 0 (A 0 (A 0 (A 1 1)))))
+(A 1 (A 0 (A 0 (A 0 2))))
+(A 1 (A 0 (A 0 4)))
+(A 1 (A 0 8))
+(A 1 16)
+(A 0 (A 1 15))
+(A 0 (A 0 (A 1 14)))
+(A 0 (A 0 (A 0 (A 1 13))))
+(A 0 (A 0 (A 0 (A 0 (A 1 12)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 11))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 10)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 9))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 8)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 7))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 6)))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1
+2)))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1
+1))))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0
+2)))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 32)))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 64))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 128)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 256))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 512)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 1024))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 2048)))))
+(A 0 (A 0 (A 0 (A 0 4096))))
+(A 0 (A 0 (A 0 8192)))
+(A 0 (A 0 16384))
+(A 0 32768)
+65536
+(A 3 3) =>
+(A 2 (A 1 2))
+(A 2 (A 0 (A 1 1)))
+(A 2 (A 0 2))
+(A 2 4)
+(A 1 (A 2 3))
+(A 1 (A 1 (A 2 2)))
+(A 1 (A 1 (A 1 (A 2 1))))
+(A 1 (A 1 (A 1 2)))
+(A 1 (A 1 (A 0 (A 1 1))))
+(A 1 (A 1 (A 0 2)))
+(A 1 (A 1 4))
+(A 1 (A 0 (A 1 3)))
+(A 1 (A 0 (A 0 (A 1 2))))
+(A 1 (A 0 (A 0 (A 0 (A 1 1)))))
+(A 1 (A 0 (A 0 (A 0 2))))
+(A 1 (A 0 (A 0 4)))
+(A 1 (A 0 8))
+(A 1 16)
+(A 0 (A 1 15))
+(A 0 (A 0 (A 1 14)))
+(A 0 (A 0 (A 0 ( A 1 13))))
+(A 0 (A 0 (A 0 ( A 0 ( A 1 12)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 11))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 10))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 9)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 8))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 7)))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 6))))))))))))
+................
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0
+1)))))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0
+2))))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4
+)))))))))))))))
+..........................
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 1024))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 2048)))))
+(A 0 (A 0 (A 0 (A 0 4096))))
+(A 0 (A 0 (A 0 8192)))
+(A 0 (A 0 16384))
+(A 0 32768)
+65536
+Now for below definitions,
+(define (f n) (A 0 n))
+(define (g n) (A 1 n))
+(define (h n) (A 2 n))
+(define (k n) (* 5 n n))
+Let’s consider (define (f n) (A 0 n))
+Here execute it with some values ,
+(f 0 ) => 0
+(f 1 ) =>2
+(f 2 ) => 4
+So in general (f n) => 2n
+Let’s consider (define (g n) (A 1 n))
+(g 0) => 0
+(g 1) => 2
+(g 2) => (A 1 2 )=> (A 0 (A 1 1)) => (A 0 2) => 4
+(g 3) => (A 1 3)=> (A 0 (A 1 2)=> (A 0 (A 1 (A 1 1))) => (A 0 (A 1 2))=>(A 0 4) => 8
+(g 4) => 16
+So in general (g n) => 2 n , when n > 0
+Let’s consider (define (h n) (A 2 n))
+(h 0) => 0
+(h 1 ) => 2 => 2 1
+(h 2 ) =>4 => 2 2
+(h 3 ) => 16 => 2 4
+( h 4 ) => 65536 => 2 16
+So in general (h n) => 2 h(n-1) , when n > 1
+we can describe above behavior using scala as follows */
+
+
+object AckermannPrinciple extends App{
+// Exercise 1.10
+def A(x: Long, y: Long): Long = {
+(x, y) match {
+case (x, 0 ) => 0
+case ( 0 , y) => y * 2
+case (x, 1 ) => 2
+case _ => A(x - 1 , A(x, y - 1 ))
+}
+}
+println( s"(A 1 10 )=> $ {A( 1 , 10 )} " )
+println( s"(A 2 4 )=> $ {A( 2 , 4 )} " )
+println( s"(A 3 3 )=> $ {A( 3 , 3 )} " )
+// procedure f(n)
+def f(n: Long) = A( 0 , n)
+//Procedure g(n)
+def g(n: Long) = A( 1 , n)
+//Procedure h(n)
+def h(n: Long) = A( 2 , n)
+// Procedure h(n)
+def k(n: Long) = 5 * n * n
+println( "================Understand the pattern of f(n)=>2n===============" )
+println(f( 0 ))
+println(f( 1 ))
+println(f( 2 ))
+println(f( 3 ))
+println(f( 4 ))
+println( "================Understand the pattern of g(n)=> 2^n=================" )
+println(g( 0 ))
+println(g( 1 ))
+println(g( 2 ))
+println(g( 3 ))
+println(g( 4 ))
+println( "=================Understand the pattern of h(n)=>2^h(n-1)================" )
+println(h( 0 ))
+println(h( 1 ))
+println(h( 2 ))
+println(h( 3 ))
+println(h( 4 ))
+}
